ee1e226ad9ba62a033981bcf2d495797edeefa20
raywang16 <bakaknife@yeah.net>
2023-10-19 16:08:28 +0800

    task 0.4 :)

--- minitorch/module.py
+++ minitorch/module.py
@@ -32,12 +32,21 @@ class Module:
     def train(self) -> None:
         "Set the mode of this module and all descendent modules to `train`."
         # TODO: Implement for Task 0.4.
-        raise NotImplementedError('Need to implement for Task 0.4')
+        self.training = True
+        if self._modules != {}:
+            for name in self._modules:
+                self._modules[name].train()
+
+        # raise NotImplementedError('Need to implement for Task 0.4')
 
     def eval(self) -> None:
         "Set the mode of this module and all descendent modules to `eval`."
         # TODO: Implement for Task 0.4.
-        raise NotImplementedError('Need to implement for Task 0.4')
+        self.training = False
+        if self._modules != {}:
+            for name in self._modules:
+                self._modules[name].eval()
+        # raise NotImplementedError('Need to implement for Task 0.4')
 
     def named_parameters(self) -> Sequence[Tuple[str, Parameter]]:
         """
@@ -48,12 +57,27 @@ class Module:
             The name and `Parameter` of each ancestor parameter.
         """
         # TODO: Implement for Task 0.4.
-        raise NotImplementedError('Need to implement for Task 0.4')
+        res = {}
+        if self._modules == {}:
+            if self._parameters != {}:
+                for name in self._parameters:
+                    res[name] = self._parameters[name]
+        else:
+            if self._parameters != {}:
+                for name in self._parameters:
+                    res[name] = self._parameters[name]
+            for name in self._modules:
+                temp = self._modules[name].named_parameters()
+                for key in temp:
+                    res[name + "." + key] = temp[key]
+        return res
+        # raise NotImplementedError('Need to implement for Task 0.4')
 
     def parameters(self) -> Sequence[Parameter]:
         "Enumerate over all the parameters of this module and its descendents."
         # TODO: Implement for Task 0.4.
-        raise NotImplementedError('Need to implement for Task 0.4')
+        return self.named_parameters().values()
+        # raise NotImplementedError('Need to implement for Task 0.4')
 
     def add_parameter(self, k: str, v: Any) -> Parameter:
         """
